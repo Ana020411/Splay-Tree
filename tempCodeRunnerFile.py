442df30cdfd22777a58590675185520f1de137c8
carolinavef <163360189+carovelasco@users.noreply.github.com>
2024-04-25 22:10:57 -0600

    minimini avance creo

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,13 +1,3 @@
-
-#HOLAAAAAAAA CREO QUE NO ESTA TAN MAL EL CODIGO  PERO SI HAY AUN ERRORES, HAY QUE CHECAR:
-#*EL CASO DE QUE HIJO DERECHO E HIJO IZQUIEDO SEAN NONE PARA LAS ROTACIONES
-#COMO SE ESTA IMPRIMIENDO PORQUE LA PRIMERA VEZ LO HACE BIEN PERO YA QUE LE METES DESPUES UN NUEVO NODO CREO QUE YA NO TANTO
-#BUENO ESO CONSIDERANDO QUE ES COMO EL BINARY TREE Y LOS NODOS MAS GRANDES VAN A LA DERECHA
-"""GRACIASSSS PERDON, ES LO UNICO QUE PUDE ADELANTAR, YA NO ALACANZO A MAS, PERO OBVIO REGRESANDO TE AYUDO EN TODO LO QUE PUEDA MIL GRACIASSS TQ"""
-# Y SI NO SIRVE ALGO, TU BORRA TODO SIN PROBLEMA :))) MUCHAS GRACIAS 
-# Y SI QUIERAS DEJAME ALGO,NO PASA NADAAA O CUALQUIER COSA ME HABLAS Y EL PRIMER LUNES LO VEOO
-#GRACIAAAAAAAAAAAAAAAS
-
 class Node:
     def __init__(self, value, parent=None) -> None:
         self.value = value
@@ -40,49 +30,50 @@ class SplayTree:
             y.left = curr_node
             curr_node.parent = y
         else:
-            return None,("checar")
+            print("Cannot perform left rotation as the right child of the current node does not exist.")
+
+
     # ---------------- ZAG ---------------------
     def right_rotation(self, curr_node):
-        if curr_node.left is not None:  # Check if the left child exists
-            y = curr_node.left
-            curr_node.left = y.right
+        y = curr_node.left # Se guarda la referencia del nodo izquierdo de curr_node en y
+        curr_node.left = y.right
 
-            if y.right is not None:  # Check if the right child of 'y' exists
-                y.right.parent = curr_node
+        if y.right != None: # Si el hijo derecho de "y" existe
+            y.right.parent = curr_node
 
-            y.parent = curr_node.parent
+        y.parent = curr_node.parent
 
-            if curr_node.parent is None:  # curr_node is root
-                self.root = y
-            elif curr_node == curr_node.parent.right:  # curr_node is right child
-                curr_node.parent.right = y
-            else:  # curr_node is left child
-                curr_node.parent.left = y
+        if curr_node.parent == None:  # curr_node es raíz
+            self.root = y
 
-            y.right = curr_node
-            curr_node.parent = y
-        else:
-            return None,("checar")
+        elif curr_node == curr_node.parent.right: # curr_node es hijo derecho
+            curr_node.parent.right = y
+
+        else: # curr_node es hijo izquierdo
+            curr_node.parent.left = y
+
+        y.right = curr_node # Se actualizan referencias
+        curr_node.parent = y
 
     # ---------------- ZIG-ZIG ------------------
     def zig_zig(self, curr_node):
-        self.right_rotation(curr_node)
-        self.right_rotation(curr_node.parent)
+        self.left_rotation(curr_node.parent)
+        self.left_rotation(curr_node)
 
     # ---------------- ZAG-ZAG ------------------
     def zag_zag(self, curr_node):
-        self.left_rotation(curr_node)
-        self.left_rotation(curr_node.parent)
+        self.right_rotation(curr_node.parent)
+        self.right_rotation(curr_node)
 
     # ---------------- ZIG-ZAG ------------------
     def zig_zag(self, curr_node):
         self.right_rotation(curr_node)
-        self.left_rotation(curr_node.parent)
+        self.left_rotation(curr_node)
 
     # ---------------- ZAG-ZIG ------------------
     def zag_zig(self, curr_node):
         self.left_rotation(curr_node)
-        self.right_rotation(curr_node.parent)
+        self.right_rotation(curr_node)
 
     #-----------Para hacerle splay despues de cada operacion y que la raiz sea el nodo usado-----------
     def splay_tree(self, node):
@@ -127,16 +118,13 @@ class SplayTree:
 
         self.splay_tree(new_node)
 
+    '''def search(self, node):
+        pass
+
+    def delete(self, node):
+        pass'''
+
 tree = SplayTree()
 tree.insert(10)
 tree.insert(20)
 tree.insert(5)
-
-# Print the root of the tree
-print(tree.root.value)
-print(tree.root.right.value)
-print(tree.root.right.left.value)
-print("----------------")
-tree.insert(4)
-
-print(tree.root.value)
