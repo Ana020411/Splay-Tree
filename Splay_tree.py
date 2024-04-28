0400a2a67a8dbfeabf500d529271cec82bb20f66
apmr11 <124747834+apmr11@users.noreply.github.com>
2024-04-28 15:50:12 -0600

    Código completo y funcionando
    Nada más es checar detalles

--- Splay_tree.py
+++ Splay_tree.py
@@ -1,13 +1,3 @@
-
-#HOLAAAAAAAA CREO QUE NO ESTA TAN MAL EL CODIGO  PERO SI HAY AUN ERRORES, HAY QUE CHECAR:
-#*EL CASO DE QUE HIJO DERECHO E HIJO IZQUIEDO SEAN NONE PARA LAS ROTACIONES
-#COMO SE ESTA IMPRIMIENDO PORQUE LA PRIMERA VEZ LO HACE BIEN PERO YA QUE LE METES DESPUES UN NUEVO NODO CREO QUE YA NO TANTO
-#BUENO ESO CONSIDERANDO QUE ES COMO EL BINARY TREE Y LOS NODOS MAS GRANDES VAN A LA DERECHA
-"""GRACIASSSS PERDON, ES LO UNICO QUE PUDE ADELANTAR, YA NO ALACANZO A MAS, PERO OBVIO REGRESANDO TE AYUDO EN TODO LO QUE PUEDA MIL GRACIASSS TQ"""
-# Y SI NO SIRVE ALGO, TU BORRA TODO SIN PROBLEMA :))) MUCHAS GRACIAS 
-# Y SI QUIERAS DEJAME ALGO,NO PASA NADAAA O CUALQUIER COSA ME HABLAS Y EL PRIMER LUNES LO VEOO
-#GRACIAAAAAAAAAAAAAAAS
-
 class Node:
     def __init__(self, value, parent=None) -> None:
         self.value = value
@@ -21,70 +11,48 @@ class SplayTree:
 
     # ---------------  ZIG -------------------
     def left_rotation(self, curr_node):
-        if curr_node.right is not None:  # Check if the right child exists
-            y = curr_node.right
-            curr_node.right = y.left 
+        y = curr_node.right
+        curr_node.right = y.left 
 
-            if y.left is not None:  # Check if the left child of 'y' exists
-                y.left.parent = curr_node
+        if y.left is not None:  # Check if the left child of 'y' exists
+            y.left.parent = curr_node
 
-            y.parent = curr_node.parent
+        y.parent = curr_node.parent
 
-            if curr_node.parent is None:  # curr_node is root
-                self.root = y
-            elif curr_node == curr_node.parent.left:  # curr_node is left child
-                curr_node.parent.left = y
-            else:  # curr_node is right child
-                curr_node.parent.right = y
+        if curr_node.parent is None:  # curr_node is root
+            self.root = y
+        elif curr_node == curr_node.parent.left:  # curr_node is left child
+            curr_node.parent.left = y
+        else:  # curr_node is right child
+            curr_node.parent.right = y
 
-            y.left = curr_node
-            curr_node.parent = y
-        else:
-            return None,("checar")
+        y.left = curr_node
+        curr_node.parent = y
+
+        
     # ---------------- ZAG ---------------------
     def right_rotation(self, curr_node):
-        if curr_node.left is not None:  # Check if the left child exists
-            y = curr_node.left
-            curr_node.left = y.right
+        y = curr_node.left
+        curr_node.left = y.right
 
-            if y.right is not None:  # Check if the right child of 'y' exists
-                y.right.parent = curr_node
+        if y.right is not None:  # Check if the right child of 'y' exists
+            y.right.parent = curr_node
 
-            y.parent = curr_node.parent
+        y.parent = curr_node.parent
 
-            if curr_node.parent is None:  # curr_node is root
-                self.root = y
-            elif curr_node == curr_node.parent.right:  # curr_node is right child
-                curr_node.parent.right = y
-            else:  # curr_node is left child
-                curr_node.parent.left = y
+        if curr_node.parent is None:  # curr_node is root
+            self.root = y
+        elif curr_node == curr_node.parent.right:  # curr_node is right child
+            curr_node.parent.right = y
+        else:  # curr_node is left child
+            curr_node.parent.left = y
 
-            y.right = curr_node
-            curr_node.parent = y
-        else:
-            return None,("checar")
+        y.right = curr_node
+        curr_node.parent = y
+   
 
-    # ---------------- ZIG-ZIG ------------------
-    def zig_zig(self, curr_node):
-        self.right_rotation(curr_node)
-        self.right_rotation(curr_node.parent)
+    #------------------------------ SPLAYING ------------------------------------
 
-    # ---------------- ZAG-ZAG ------------------
-    def zag_zag(self, curr_node):
-        self.left_rotation(curr_node)
-        self.left_rotation(curr_node.parent)
-
-    # ---------------- ZIG-ZAG ------------------
-    def zig_zag(self, curr_node):
-        self.right_rotation(curr_node)
-        self.left_rotation(curr_node.parent)
-
-    # ---------------- ZAG-ZIG ------------------
-    def zag_zig(self, curr_node):
-        self.left_rotation(curr_node)
-        self.right_rotation(curr_node.parent)
-
-    #-----------Para hacerle splay despues de cada operacion y que la raiz sea el nodo usado-----------
     def splay_tree(self, node):
         while node.parent is not None:
             if node.parent == self.root:
@@ -93,50 +61,164 @@ class SplayTree:
                 else:
                     self.left_rotation(node.parent)
             else:
-                parent = node.parent
-                g = parent.parent
-                if parent.left == node and g.left == parent:
-                    self.zig_zig(node)
-                elif parent.right == node and g.right == parent:
-                    self.zag_zag(node)
-                elif parent.right == node and g.left == parent:
-                    self.zig_zag(node)
+                parent_node = node.parent
+                grand_parent = parent_node.parent
+
+                if node == parent_node.left and parent_node == grand_parent.left:
+                    # ---------------- ZIG - ZIG --------------------
+                    self.right_rotation(grand_parent)
+                    self.right_rotation(parent_node)
+                elif node == parent_node.right and parent_node == grand_parent.right:
+                    # ----------------- ZAG - ZAG ---------------------
+                    self.left_rotation(grand_parent)
+                    self.left_rotation(parent_node)
+                elif node == parent_node.right  and parent_node == grand_parent.left:
+                    # ----------------- ZAG - ZIG ---------------------
+                    self.left_rotation(parent_node)
+                    self.right_rotation(grand_parent)
                 else:
-                    self.zag_zig(node)
+                    # ----------------- ZIG - ZAG ----------------------
+                    self.right_rotation(parent_node)
+                    self.left_rotation(grand_parent)
+
         self.root = node
+    
 
-    def insert(self, value):
-        new_node = Node(value)
-        if self.root is None:
-            self.root = new_node
-        else:
-            raiz = self.root
-            parent = None
-            while raiz is not None:
-                parent = raiz
-                if value < raiz.value:
-                    raiz = raiz.left
-                else:
-                    raiz = raiz.right
+    # --------------------------------------------- INSERCIÓN -------------------------------------------
+    def insert(self, node):
+        new_node = Node(node, parent=None)
+        
+        raiz = self.root
+        y = None
 
-            new_node.parent = parent
-            if value < parent.value:
-                parent.left = new_node
+        while raiz is not None:
+            y = raiz
+            if new_node.value < raiz.value:
+                raiz = raiz.left
             else:
-                parent.right = new_node
+                raiz = raiz.right
+
+        new_node.parent = y
+
+        if y is None:
+            self.root = new_node
+        elif new_node.value < y.value:
+            y.left = new_node
+        else:
+            y.right = new_node
 
         self.splay_tree(new_node)
 
+
+    # ------------------------------------------------ BÚSQUEDA ------------------------------------------
+    def search(self, value):
+        if self.root is None: # El árbol está vacío
+            return "El árbol está vacío"
+        
+        curr_node = self.root
+        while curr_node is not None:
+            if value == curr_node.value:
+                self.splay_tree(curr_node)
+                return f"Valor {curr_node.value} encontrado"
+            elif value < curr_node.value:
+                curr_node = curr_node.left
+            elif value > curr_node.value:
+                curr_node = curr_node.right
+        
+        return "No se encontró"
+    
+
+    # ------------------------------------------ ELIMINACIÓN ------------------------------------------
+    def delete(self, value):
+        # Top - down 
+        node = self.search_node(value)
+
+        if self.root is None:
+            return None
+        
+        self.splay_tree(node) # Para que quede como raíz
+
+        left_subtree = SplayTree()
+        left_subtree.root = self.root.left
+
+        if left_subtree.root is not None:
+            left_subtree.root.parent = None # Se elimina la raíz, que en este caso es node
+
+        right_subtree = SplayTree()
+        right_subtree.root = self.root.right
+
+        if right_subtree.root is not None:
+            right_subtree.root.parent = None # Se quitan las referencias al node para que se elimine
+
+        if left_subtree.root is not None:
+            max_node = left_subtree.max(left_subtree.root) # Se saca el nodo con mayor valor del subárbol izquierdo
+            left_subtree.splay_tree(max_node)
+            left_subtree.root.right = right_subtree.root # Se une el subarbol derecho y el izquierdo
+            self.root = left_subtree.root
+
+        else:
+            self.root = right_subtree.root
+
+    # ------------------------------------- FUNCIONES DEL DELETE -----------------------------
+    def search_node(self, value):
+        if self.root is None: # El árbol está vacío
+            return None
+        
+        curr_node = self.root
+        while curr_node is not None:
+            if value == curr_node.value:
+                self.splay_tree(curr_node)
+                return curr_node
+            elif value < curr_node.value:
+                curr_node = curr_node.left
+            elif value > curr_node.value:
+                curr_node = curr_node.right
+        
+        return None
+
+    def max(self, node):
+        if node is None:
+            return False
+        
+        while node.right is not None:
+            node = node.right
+
+        return node
+
+    # ------------------------------------- IMPRESIÓN DEL ÁRBOL ---------------------------------
+    def inorder(self):
+        self.__inorder(self.root)
+
+    def __inorder(self, current_node):
+        if current_node.left is not None:
+            self.__inorder(current_node.left)
+
+        print(current_node.value)
+
+        if current_node.right is not None:
+            self.__inorder(current_node.right)
+
+
+# --------------------------------- CASO PRUEBA--------------------------------------
 tree = SplayTree()
 tree.insert(10)
+tree.insert(8)
 tree.insert(20)
 tree.insert(5)
+#tree.insert(4)
+#tree.insert(3)
 
-# Print the root of the tree
+print("Raíz:")
 print(tree.root.value)
-print(tree.root.right.value)
-print(tree.root.right.left.value)
-print("----------------")
-tree.insert(4)
+print("Recorrido inorder del árbol:")
+print(tree.inorder())
+print("¿Se encontró?")
+print(tree.search(8))
+print("--------------------------------------------------------")
+tree.delete(8)
+print("Raíz:")
+print(tree.root.value)
+print("Recorrido inorder del árbol sin el eliminado:")
+print(tree.inorder())
 
-print(tree.root.value)
+
